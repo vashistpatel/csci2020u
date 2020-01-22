5e0889173d5731dd8e209ebf62218c812bdd3f0a
vashist patel <vashist.patel@ontariotechu.net>
2020-01-22 16:06:21 -0500

    finished part 2

--- lab02/src/main/java/csci2020u/SimpleTest.java
+++ lab02/src/main/java/csci2020u/SimpleTest.java
@@ -3,3 +3,32 @@ public class SimpleTest{
     System.out.println("testttttttttts");
   }
 }
+// apply plugin: 'java'
+//
+// repositories{
+//   mavenCentral();
+// }
+//
+// dependencies{
+//   compile 'org.apache.commons:commons-csv:1.5'
+// }
+//
+// task execute(type:JavaExec, dependsOn: 'classes'){
+//   main = 'SimpleTest'
+//   classpath = sourceSets.main.runtimeClasspath
+// }
+
+
+
+// plugins {
+//     id "application"
+// }
+// apply plugin: 'java'
+//
+// mainClassName = "gradleDemo.Demo"
+// repositories {
+//     mavenCentral()
+//  }
+//  dependencies {
+//    implementation 'org.apache.commons:commons-csv:1.7'
+//  }
